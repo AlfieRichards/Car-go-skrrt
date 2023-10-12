3afafc1d3650cd8f0d5fb5dbb11fbb17dfadebb3
Alfie <87566534+AlfieRichards@users.noreply.github.com>
2023-10-12 12:09:35 +0100

    fixed issues

--- Assets/EnemyRotFix.cs
+++ Assets/EnemyRotFix.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class EnemyRotFix : MonoBehaviour
 {
-    public Transform target;
+    public Transform sphere; // Reference to the sphere GameObject
+    public float rotationSpeed = 10f;
+    public Vector3 rotOffset;
 
     private void Start() {
-        target = transform.parent.gameObject.transform;
+        sphere = transform.parent.gameObject.transform;
     }
 
-    void Update()
+    private void Update()
     {
-        // Rotate the camera every frame so it keeps looking at the target
-        transform.LookAt(target);
+        // Calculate the direction from the sphere to the rotating object
+        Vector3 toSphere = sphere.position - transform.position;
+
+        // Calculate the rotation that aligns the object's forward vector with toSphere
+        Quaternion targetRotation = Quaternion.LookRotation(toSphere);
+
+        // Invert the rotation to prevent the object from being upside down
+        targetRotation *= Quaternion.Euler(rotOffset);
+
+        // Smoothly interpolate between the current rotation and the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        // Rotate the object around the sphere
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
     }
 }
